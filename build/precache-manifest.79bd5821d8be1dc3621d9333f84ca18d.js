f4366ac7097011d1be9512a03dc4ea52d8524e41
Nursultan Yermagambetov <44144413+dzincun@users.noreply.github.com>
2020-08-20 00:11:16 +0600

    Add files via upload

---
+++ build/precache-manifest.79bd5821d8be1dc3621d9333f84ca18d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327f8a4c6e19bbe75f79e27b34d9d32c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feaa7ab54cf8918a2e67",
+    "url": "/static/css/main.c685f35f.chunk.css"
+  },
+  {
+    "revision": "f3ed12bffeb81b8fce92",
+    "url": "/static/js/2.3fd06d78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3fd06d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feaa7ab54cf8918a2e67",
+    "url": "/static/js/main.507944b6.chunk.js"
+  },
+  {
+    "revision": "51d2a0dde69bf6c18b92",
+    "url": "/static/js/runtime-main.69fb69c6.js"
+  }
+]);
